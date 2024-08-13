4d25b5e4c327d56e1f45b2ba2f142f100de1cd33
Martin Otero <martin_otero@hotmail.com>
2024-08-13 12:15:17 -0300

    6

--- src/app/components/step.js
+++ src/app/components/step.js
@@ -3,14 +3,31 @@ import React, { useState } from "react";
 
 export default function Step(props) {
   return (
-    <div className="flex flex-col w-[500px] h-[550px] border-2 border-red-700 text-blue-900 m ">
-      <h1 className="text-3xl font-bold mt-4 ">Personal info</h1>
-      <p className="text-lg text-gray-400 mt-1 mb-2">
-        Please provide your name, email adress, and phone number.
-      </p>
-      <div className="mt-4">
-        <p className="text-blue-900 font-medium">Name</p>
-        <input className="rounded-md border-2 border-gray-400 focus:border-blue-800 focus"></input>
+    <div className="flex flex-col w-[500px] h-[550px] border-0 text-blue-900  py-4 place-content-between">
+      <div className="flex flex-col mt-4 ">
+        <h1 className="text-3xl font-bold  ">Personal info</h1>
+        <p className="text-lg text-gray-400 mt-1 ">
+          Please provide your name, email adress, and phone number.
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col ">
+          <p className="font-medium">Name</p>
+          <input className="rounded-md border-[1px] mt-1 border-gray-400 focus:border-blue-800 focus h-[45px] w-full p-3 text-lg font-semibold"></input>
+        </div>
+        <div className="flex flex-col mt-6 ">
+          <p className="font-medium">Email Address</p>
+          <input className="rounded-md border-[1px] mt-1 border-gray-400 focus:border-blue-800 focus h-[45px] w-full p-3 text-lg font-semibold"></input>
+        </div>
+        <div className="flex flex-col mt-6 ">
+          <p className="font-medium">Phone Number</p>
+          <input className="rounded-md border-[1px] mt-1 border-gray-400 focus:border-blue-800 focus h-[45px] w-full p-3 text-lg font-semibold"></input>
+        </div>
+      </div>
+      <div className="flex mt-8 w-full  place-content-end ">
+        <button className="bg-sky-900 rounded-md h-[50px] w-[120px] text-white font-medium">
+          Next Step
+        </button>
       </div>
     </div>
   );
